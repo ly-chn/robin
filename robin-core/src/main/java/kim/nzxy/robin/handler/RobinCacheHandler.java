2291d9a7d23a2f9684016a8d34d3b05bbac549c5
xy <3293232930@qq.com>
2021-06-10 01:23:07 +0800

    add clean fun

--- robin-core/src/main/java/kim/nzxy/robin/handler/RobinCacheHandler.java
+++ robin-core/src/main/java/kim/nzxy/robin/handler/RobinCacheHandler.java
@@ -1,6 +1,8 @@
 package kim.nzxy.robin.handler;
 
 
+import kim.nzxy.robin.enums.RobinRuleEnum;
+
 import java.time.Duration;
 import java.util.List;
 
@@ -12,19 +14,24 @@ import java.util.List;
  * @since 2021/6/4
  */
 public interface RobinCacheHandler {
-    /**
-     * ip访问记录
-     *
-     * @param ip        ip地址
-     * @param timestamp 访问时间, 秒级时间戳
-     */
-    void ipAccessRecord(String ip, int timestamp);
 
     /**
-     * @param ip ip地址
-     * @return 该 ip 地址的最近访问时间集合
+     * 添加访问记录
+     *
+     * @param type      访问类型, 归属于保存记录的策略
+     * @param target    访客
+     * @param timestamp 访问时间, 秒级时间戳
      */
-    List<Integer> ipAccessRecord(String ip);
+    void accessRecord(RobinRuleEnum type, String target, int timestamp);
+
+    /**
+     * 获取全部访问记录
+     *
+     * @param type   访问类型, 归属于保存记录的策略
+     * @param target 访客
+     */
+    List<Integer> accessRecord(RobinRuleEnum type, String target);
+
 
     /**
      * 封禁ip
@@ -41,4 +48,44 @@ public interface RobinCacheHandler {
      * @return 返回 true 表示 ip 已被封
      */
     boolean lockIp(String ip);
+
+    /**
+     * 设置锁定
+     *
+     * @param type   锁定原因
+     * @param target 锁定目标
+     * @param unlock 锁定时间
+     */
+    void lock(RobinRuleEnum type, String target, Duration unlock);
+
+    /**
+     * 判断是否已被锁定
+     *
+     * @param type   锁定原因
+     * @param target 锁定内容
+     * @return 为true表示已锁定
+     */
+    boolean lock(RobinRuleEnum type, String target);
+
+    /**
+     * 主动解除锁定
+     *
+     * @param type   锁定原因
+     * @param target 锁定类型
+     */
+    void unlock(RobinRuleEnum type, String target);
+
+    /**
+     * 清理访问记录, 删除指定时间戳之前的日志记录
+     *
+     * @param type      访问类型, 归属于保存记录的策略
+     * @param timestamp 访问时间, 秒级时间戳, 低于此时间的都将会被清理
+     */
+    void cleanAccessRecord(RobinRuleEnum type, int timestamp);
+
+
+    /**
+     * 清理锁定内容
+     */
+    void cleanLock();
 }
