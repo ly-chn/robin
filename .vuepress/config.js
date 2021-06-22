7afcd7855e0e66c3be0c389103888b94a591d062
xy <3293232930@qq.com>
2021-06-22 20:19:39 +0800

    doc?

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,18 +1,18 @@
 module.exports = {
     dest: 'docs',
-    base: '/docs-robin/',
-    title: 'Robin',
+    base: '/robin/',
+    title: 'Robin - 期望成为一个反爬框架',
     description: '期望成为一个反爬框架',
     themeConfig: {
         repo: 'ly-chn/robin',
         docsDir: 'robin-doc',
-        docsBranch: 'master',
+        docsBranch: 'docs',
         editLinks: true,
         locales: {
             '/': {
                 label: '简体中文',
                 selectText: 'Languages',
-                editLinkText: '在 GitHub 上编辑此页',
+                // editLinkText: '在 GitHub 上编辑此页',
                 lastUpdated: '上次更新',
                 nav: [{
                     text: "指南",
