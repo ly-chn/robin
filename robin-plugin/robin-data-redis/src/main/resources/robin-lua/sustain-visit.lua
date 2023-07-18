65117a9a365d9a958d6a0395f50462a5297e27d0
ly-chn <3293232930@qq.com>
2023-07-18 10:57:46 +0800

    lua脚本精度调整

--- robin-plugin/robin-data-redis/src/main/resources/robin-lua/sustain-visit.lua
+++ robin-plugin/robin-data-redis/src/main/resources/robin-lua/sustain-visit.lua
@@ -1,9 +1,9 @@
 -- 返回值: boolean, 为true表示校验通过
-local key = KEYS[1]
-local value = ARGV[1]
+local topic = KEYS[1]
+local metadata = ARGV[1]
 local logEnabled = ARGV[5] == 'true'
 if logEnabled then
-    redis.log(redis.LOG_WARNING, 'sustain-visit.lua started', KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], KEYS, ARGV)
+    redis.log(redis.LOG_WARNING, 'sustain-visit.lua started, args: ', KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], KEYS, ARGV)
 end
 -- 持续访问记录最大记录数量
 local sustainVisitPrecision = tonumber(ARGV[4])
@@ -11,31 +11,33 @@ local sustainVisitPrecision = tonumber(ARGV[4])
 local sustainVisitStep = 1 / sustainVisitPrecision
 local currentTimeFrame = tonumber(ARGV[2])
 
-local latestVisit = tonumber(redis.call('ZSCORE', key, value))
+local latestVisit = tonumber(redis.call('ZSCORE', topic, metadata))
 -- 非连续访问
 if not latestVisit or currentTimeFrame - latestVisit > 1 then
-    redis.call('ZADD', key, currentTimeFrame + sustainVisitStep, value)
+    redis.call('ZADD', topic, currentTimeFrame + sustainVisitStep, metadata)
     if logEnabled then
-        redis.log(redis.LOG_WARNING, 'latestVisit of metadata', key, ' is zero, set to', currentTimeFrame, sustainVisitStep)
+        redis.log(redis.LOG_WARNING, 'topic: ', topic, 'metadata: ', metadata, ' is zero, set to', currentTimeFrame, sustainVisitStep)
     end
     return true
 end
 -- 不同窗口
 if currentTimeFrame > latestVisit then
     latestVisit = latestVisit + 1 + sustainVisitStep
-
 end
 -- 最大可访问次数
 local maxTimes = tonumber(ARGV[3])
 
-local visitedTimes = latestVisit % 1 * sustainVisitPrecision
+local visitedTimes = math.floor(latestVisit % 1 * sustainVisitPrecision)
 -- 达到访问限制
 if visitedTimes > maxTimes then
+    if logEnabled then
+        redis.log(redis.LOG_WARNING, 'topic: ', topic, 'metadata: ', metadata, 'limited: visitedTimes', visitedTimes, 'maxTimes: ', maxTimes)
+    end
     return false
 end
 
-redis.call("ZADD", key, latestVisit, value)
+redis.call("ZADD", topic, latestVisit, metadata)
 if logEnabled then
-    redis.log(redis.LOG_WARNING, 'latestVisit of metadata', key, ' is update to, hasVisitedTimes', latestVisit)
+    redis.log(redis.LOG_WARNING, 'latestVisit of metadata', topic, metadata, ' is update to, hasVisitedTimes', latestVisit)
 end
 return true
